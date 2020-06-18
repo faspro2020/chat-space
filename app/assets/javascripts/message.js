43f900e29503e1c2253e43201f13ecb5d298cabb
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-06-18 12:47:16 +0900

    finished create html

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Chat-message">
+          <div class="Chat-name">
+            <div class="Chat-name__box">
+              ${message.user_name}
+            </div>
+          </div>
+          <div class="Chat-date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Chat-comment">
+          <div class="Chat-comment__content">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Chat-message">
+        <div class="Chat-name">
+          <div class="Chat-name__box">
+            ${message.user_name}
+          </div>
+        </div>
+        <div class="Chat-date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="Chat-comment">
+        <div class="Chat-comment__content">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+  return html;
+    };
+  }
+  
+  $('.Form-all').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Chat-main__message-list').append(html);
+      $('.Form-all')[0].reset();
+    })
+  });
+});
